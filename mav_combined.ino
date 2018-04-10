04d9d7f1f36d849fa6eb517ee58c980453cf0c33
askmuhsin <askmuhsin@gmail.com>
2018-04-10 21:21:41 +0800

    refactor, mav logic update

--- mav_combined.ino
+++ mav_combined.ino
@@ -3,31 +3,34 @@
 Servo left_servo;
 int pos = 0;
 
-int delay_read;
+int delay_val;
+const int max_angle=60;  //throttle flap max angle, default 60
 
 double channel_3; //throttle channel -- connect to reciever channel 4
-int throttle_level;
 
 void setup() {
   pinMode(3, INPUT);  //throttle logic, channel_3 >> pin_3
   left_servo.attach(9);  //servo signal line >> pin_9
-  left_servo.write(60);   
+  left_servo.write(60);
   Serial.begin(9600);
 }
 
 void loop() {
   channel_3 = pulseIn(3, HIGH);
-  delay_read = throttleLevel(channel_3);
-  flap(delay_read);
+  delay_val = throttleLevel(channel_3);
+  if (delay_val>1250)
+    flap(delay_val);
+  else
+    ;
 }
 
 void flap(int delay_) {
-  for(pos=0; pos<=60; pos+=1)
+  for(pos=0; pos<=max_angle; pos+=1)
   {
     left_servo.write(pos);
     delay(delay_);
   }
-  for(pos=60; pos>=0; pos-=1)
+  for(pos=max_angle; pos>=0; pos-=1)
   {
     left_servo.write(pos);
     delay(delay_);
@@ -35,12 +38,8 @@ void flap(int delay_) {
 }
 
 int throttleLevel(double channel) {
-    if (channel>=1750 && channel<2000)
-    return 2;
-  else if (channel>=1500 && channel<1750)
-    return 3;
-  else if (channel>=1250 && channel<1500)
-    return 4;
-  else if (channel<1250)
-    return 5;
+  int throttle_l = 0;
+  throttle_l = map(channel, 1000, 2000, -5, -2);
+  throttle_l*=-1;
+  return throttle_l;
 }
